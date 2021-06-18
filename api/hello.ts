03402abae6a38e01a531cb19cdd40fa83671c188
kawarimidoll <kawarimidoll+git@gmail.com>
2021-06-18 13:28:08 +0900

    add deno script

---
+++ api/hello.ts
@@ -0,0 +1,24 @@
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
+  Context,
+} from "../deps.ts";
+
+// deno-lint-ignore require-await
+export async function handler(
+  event: APIGatewayProxyEventV2,
+  _context: Context,
+): Promise<APIGatewayProxyResultV2> {
+  return {
+    statusCode: 200,
+    headers: { "content-type": "text/html;charset=utf8" },
+    body: JSON.stringify(
+      {
+        message: `Welcome to deno ${Deno.version.deno} 🦕`,
+        input: event,
+      },
+      null,
+      2,
+    ),
+  };
+}
---
+++ deps.ts
@@ -0,0 +1,7 @@
+import {
+  APIGatewayProxyEventV2,
+  APIGatewayProxyResultV2,
+  Context,
+} from "https://deno.land/x/lambda/mod.ts";
+
+export type { APIGatewayProxyEventV2, APIGatewayProxyResultV2, Context };
